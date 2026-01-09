3c7c33fb3368da50a4ac5c338a20b532be5a95ed
PrincyMaheshwari <princymaheshwari069@gmail.com>
2026-01-08 21:39:35 -0500

    Added cost function to logistic Regression Model

--- Course1_DeepLearningAndNeuralNetworks/logistic_regression_from_scratch.py
+++ Course1_DeepLearningAndNeuralNetworks/logistic_regression_from_scratch.py
@@ -22,3 +22,17 @@ def predict(X: np.ndarray, w: np.ndarray, b: float, threshold: float = 0.5) -> n
     probabilities = predict_probability(X, w, b)
     predicted_class_labels = (probabilities >= threshold).astype(int)
     return predicted_class_labels
+
+# Step 4: Creating a function that computes the cost function of logistic regression to 
+# find the average error in the predicted probabilities of y and use it to learn the parameters w and b.
+
+def cost_function(y_true : np.ndarray, y_predicted_probability: np.ndarray) -> float:
+
+    epsilon = 1e-12 # Used to avoid log(0)
+    y_predicted_probability = np.clip(y_predicted_probability, epsilon, 1-epsilon) # Forces all predicted probabilities of y to be between epsilon and 1-epsilon
+
+    loss_function = (y_true * np.log(y_predicted_probability)) + ((1-y_true) * np.log(1 - y_predicted_probability))
+    avg_error = float(np.mean(loss_function))
+
+    return avg_error
+
